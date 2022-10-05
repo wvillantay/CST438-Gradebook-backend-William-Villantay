f9a6096d0a19a7350bd21092a7c8f6a2d1bb2f76
wvillantay <48138214+wvillantay@users.noreply.github.com>
2022-10-05 21:47:01 +0000

    Rest
    first commit

--- cst438_gradebook_backend-master/cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
+++ cst438_gradebook_backend-master/cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
@@ -27,6 +27,7 @@ public class EnrollmentController {
 	 * endpoint used by registration service to add an enrollment to an existing
 	 * course.
 	 */
+	#testing
 	@PostMapping("/enrollment")
 	@Transactional
 	public EnrollmentDTO addEnrollment(@RequestBody EnrollmentDTO enrollmentDTO) {
